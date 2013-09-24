dfa1a0bb8f726d74aa33b5d0c7e57f86f3f17eb4
Daniel P. Seemuth <dps-github-waveforms@seemuth.com>
2013-09-24 14:11:57 -0500

    Nicely format HTML output

--- waveform.js
+++ waveform.js
@@ -3,6 +3,11 @@ var cols = 10;
 var table;
 var edit_border = "thin dotted grey";
 
+function text(str)
+{
+    return document.createTextNode(str);
+}
+
 function init()
 {
     table = document.getElementById("wftable");
@@ -13,14 +18,20 @@ function addRows(numrows)
 {
     numrows += rows;
 
+    if (table.tBodies.length < 1) {
+	table.appendChild(document.createElement('tbody'));
+    }
+
     for (; rows < numrows; rows++) {
+	table.tBodies[table.tBodies.length - 1].appendChild(text("\n"));
 	var row = table.insertRow(-1);
+	row.appendChild(text("\n"));
 
 	for (c = 0; c < cols; c++) {
 	    var cell = row.insertCell(-1);
 	    cell.innerHTML = rows.toString().concat(",", c.toString());
 	    cell.style.border = edit_border;
-	    row.appendChild(document.createTextNode("\n"));
+	    row.appendChild(text("\n"));
 	}
     }
 }
@@ -36,7 +47,7 @@ function addCols(numcols)
 	    var cell = row.insertCell(-1);
 	    cell.innerHTML = r.toString().concat(",", c.toString());
 	    cell.style.border = edit_border;
-	    row.appendChild(document.createTextNode("\n"));
+	    row.appendChild(text("\n"));
 	}
     }
 }
