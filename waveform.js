5cbf664ceab89cb517aa223246e222319a991977
Daniel P. Seemuth <dps-github-waveforms@seemuth.com>
2013-10-01 13:35:57 -0500

    Add support for selecting signal data cells

--- waveform.js
+++ waveform.js
@@ -31,9 +31,31 @@ var VERSION = 'v0.2.0';
 var START_SIGNALS = 4;
 var START_COLS = 8;
 
+var COLOR_SELECT = 'cyan';
+
 var signals = 0;
 var cols = 0;
 var table;
+
+var selected = [];
+
+
+/**
+ * Find an object within an array.
+ * @param {array} arr Array to search.
+ * @param {object} seek Object for which to search.
+ * @return {number} Index of found object, or -1 if not found.
+ */
+function indexOf(arr, seek)
+{
+    for (var i = 0; i < arr.length; i++) {
+	if (arr[i] === seek) {
+	    return i;
+	}
+    }
+
+    return -1;
+}
 
 
 /**
@@ -93,11 +115,13 @@ function setCellEventCallbacks_(cell, rowIndex, colIndex)
 	/* Header row. */
 
 	if (colIndex > 0) {
+	    cell.onclick = cell_click;
 	    cell.ondblclick = cell_dblclick;
 	}
 
     } else if (((rowIndex - 1) % 2) == 1) {
 	/* Signal row. */
+	cell.onclick = cell_click;
 	cell.ondblclick = cell_dblclick;
     }
 }
@@ -253,6 +277,19 @@ function rowToRowIndex_(row)
 
 
 /**
+ * @private
+ * Find the cell at the given table coordinates.
+ * @param {number} rowIndex Zero-based row index.
+ * @param {number} colIndex Zero-based column index.
+ * @return {cell} Cell at the given coordinates.
+ */
+function tableCoordsToCell_(rowIndex, colIndex)
+{
+    return table.rows[rowIndex].cells[colIndex];
+}
+
+
+/**
  * Add signal at the given index.
  * @param {number} index Add signal at this index (negative means last).
  */
@@ -394,6 +431,54 @@ function enableSigEdit_(enable)
 }
 
 
+/**
+ * Clear selection and unhighlight cells.
+ */
+function clearSelection_()
+{
+    for (var i = 0; i < selected.length; i++) {
+	var parts = selected[i].split('x');
+	var rowIndex = new Number(parts[0]);
+	var colIndex = new Number(parts[1]);
+	var cell = tableCoordsToCell_(rowIndex, colIndex);
+
+	cell.style.backgroundColor = '';
+    }
+
+    selected = [];
+}
+
+
+/**
+ * @private
+ * Toggle cell selection.
+ * @param {cell} cell Select/deselect this cell.
+ */
+function toggleCellSelection_(cell)
+{
+    var rowIndex = rowToRowIndex_(cell.parentNode);
+    var colIndex = cellToColIndex_(cell);
+    var cellKey = rowIndex.toString().concat('x', colIndex.toString());
+
+    var index = indexOf(selected, cellKey);
+
+    if (index < 0) {
+	/* Select this cell. */
+	selected.push(cellKey);
+
+	cell.style.backgroundColor = COLOR_SELECT;
+
+    } else {
+	/* Deselect this cell. */
+	selected.splice(index, 1);
+
+	cell.style.backgroundColor = '';
+    }
+
+    enableSigEdit_(selected.length > 0);
+}
+
+
 
 
 /**
@@ -435,6 +520,30 @@ function exportHTML()
     var io = document.getElementById('io');
     var text = table.innerHTML.trim().replace(/\n+/g, '\n');
     io.value = text;
+}
+
+
+/**
+ * Handle single-click event on a cell.
+ */
+function cell_click(event)
+{
+    var cell = event.currentTarget;
+    var rowIndex = rowToRowIndex_(cell.parentNode);
+    var colIndex = cellToColIndex_(cell);
+    var modifier = (event.altKey || event.ctrlKey || event.shiftKey);
+
+    if ((colIndex > 0) && (rowIndex > 0)) {
+	/* Signal data cell. */
+
+	if (! modifier) {
+	    /* Clear other selection and select this cell. */
+	    clearSelection_();
+	}
+
+	/* Toggle this cell's selection. */
+	toggleCellSelection_(cell);
+    }
 }
 
 
