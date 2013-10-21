57445c6bbab2fde9a4e23ee96ddd089221262a7e
Daniel P. Seemuth <dps-github-waveforms@seemuth.com>
2013-10-21 12:11:36 -0500

    Bump version to 0.4.0

--- waveform.js
+++ waveform.js
@@ -23,11 +23,11 @@
  * DEALINGS IN THE SOFTWARE.
  *
  * @author Daniel P. Seemuth
- * @version 0.3.3
+ * @version 0.4.0
  **/
 
 var PROJ_NAME = 'Waveform Editor';
-var VERSION = 'v0.3.3';
+var VERSION = 'v0.4.0';
 
 var START_SIGNALS = 4;
 var START_COLS = 8;
