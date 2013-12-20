4d2d60005e75b5717d65cdb35164591623fb17b6
Daniel P. Seemuth <dps-github-waveforms@seemuth.com>
2013-12-19 21:49:26 -0600

    Release v0.9.0: import waveform data back from exported text

--- waveform.js
+++ waveform.js
@@ -23,11 +23,11 @@
  * DEALINGS IN THE SOFTWARE.
  *
  * @author Daniel P. Seemuth
- * @version 0.8.0
+ * @version 0.9.0
  **/
 
 var PROJ_NAME = 'Waveform Editor';
-var VERSION = 'v0.8.0';
+var VERSION = 'v0.9.0';
 
 var START_SIGNALS = 4;
 var START_COLS = 8;
