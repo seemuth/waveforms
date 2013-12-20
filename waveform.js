35859be011fc01704f3f91abe55e7714e4ebb624
Daniel P. Seemuth <dps-github-waveforms@seemuth.com>
2013-12-19 22:53:58 -0600

    Release v0.9.2: miscellaneous improvements
    Fix bug in error message for invalid data values
    Improve UI for signal name editing

--- waveform.js
+++ waveform.js
@@ -23,11 +23,11 @@
  * DEALINGS IN THE SOFTWARE.
  *
  * @author Daniel P. Seemuth
- * @version 0.9.1
+ * @version 0.9.2
  **/
 
 var PROJ_NAME = 'Waveform Editor';
-var VERSION = 'v0.9.1';
+var VERSION = 'v0.9.2';
 
 var START_SIGNALS = 4;
 var START_COLS = 8;
