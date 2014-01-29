f4d8218819deccbcbea62bed884b74951bc09e97
Daniel P. Seemuth <dps-github-waveforms@seemuth.com>
2014-01-29 14:26:32 -0600

    Release v0.12.0: Split exported waveform and data/settings
    Separate HTML textarea into waveform and data/settings textareas
    Export and import settings as well as signal data
    Backwards-incompatibility: data export tag reads DATA instead of DATA_EXPORT

--- waveform.js
+++ waveform.js
@@ -23,11 +23,11 @@
  * DEALINGS IN THE SOFTWARE.
  *
  * @author Daniel P. Seemuth
- * @version 0.11.2
+ * @version 0.12.0
  **/
 
 var PROJ_NAME = 'Waveform Editor';
-var VERSION = 'v0.11.2';
+var VERSION = 'v0.12.0';
 
 var START_SIGNALS = 4;
 var START_COLS = 8;
